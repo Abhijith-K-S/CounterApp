91630366fabd651b85e8a29809d8ee0867e4298e
Abhijith K S <abhijithks1729@gmail.com>
2022-10-02 20:53:16 +0530

    Final App

--- app/src/main/java/com/example/counterapp/MainActivity.kt
+++ app/src/main/java/com/example/counterapp/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.counterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val counter = findViewById<TextView>(R.id.counter)
+        val minus = findViewById<Button>(R.id.minus)
+        val plus = findViewById<Button>(R.id.plus)
+
+        minus.setOnClickListener {
+           var text = counter.text.toString().toInt()
+            text -= 1
+           counter.text = text.toString()
+        }
+
+        plus.setOnClickListener {
+            var text = counter.text.toString().toInt()
+            text += 1
+            counter.text = text.toString()
+
+            if(text==10) {
+                val toastText = Toast.makeText(this, "Value is 10", Toast.LENGTH_SHORT)
+                toastText.show()
+            }
+        }
     }
 }
